2ab1216ae2998827d395e98bb51a19246dba378b
Hamilton Posada Serna <posadahamilton@gmail.com>
2023-10-02 23:23:36 +0200

    I maked new components such as logo.js and navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,36 @@
+import Logo from './logo'
+import NextLink from 'next/link'
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+
+const LinkItem = ({ href, path, children }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+  return (
+    <Link
+      as={NextLink}
+      href={href}
+      scroll={false}
+      p={2}
+      bg={active ? 'grassTeal' : undefined}
+      color={active ? '#202023' : inactiveColor}
+      target={target}
+      {...props}
+    >
+      {children}
+    </Link>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius={'lg'} bg="red" p={3} mb={6} alignSelf={'center'}>
+        Hello, I&pos;m a fullstack developer based in Amsterdam!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as={'h2'} variant={'page-title'}>
+            Hamilton Posada
+          </Heading>
+          <p>Digital Craftzman (Artis / Developer / Designer)</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
