a49265b3c12393f1718062ea555cd6cdbafa0233
Hamilton Posada Serna <posadahamilton@gmail.com>
2023-10-12 13:55:14 +0200

    Added my profile image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Box, Container, Heading } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius={'lg'} bg="red" p={3} mb={6} alignSelf={'center'}>
+      <Box
+        borderRadius={'lg'}
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        alignSelf={'center'}
+      >
         Hello, I&pos;m a fullstack developer based in Amsterdam!
       </Box>
 
@@ -13,6 +25,23 @@ const Page = () => {
             Hamilton Posada
           </Heading>
           <p>Digital Craftzman (Artis / Developer / Designer)</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/hamilton.jpg"
+            alt="Profile image of Hamilton Posada"
+          />
         </Box>
       </Box>
     </Container>
