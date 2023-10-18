5088e5de6648a9d02676cd3f7f4947971e6f7fe1
Hamilton Posada Serna <posadahamilton@gmail.com>
2023-10-18 12:59:58 +0200

    add work page

---
+++ pages/works.js
@@ -0,0 +1,14 @@
+import { Container, Divider, Heading, SimpleGrid } from '@chakra-ui/react'
+// import Section from '../components/Sextion'
+
+const Works = () => {
+  return (
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+    </Container>
+  )
+}
+
+export default Works
