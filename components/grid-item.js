00cb9acfd420ede903127487c943d17510a04d71
Hamilton Posada Serna <posadahamilton@gmail.com>
2023-10-18 15:08:17 +0200

    added the work page

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,15 +1,15 @@
 import NextLink from 'next/link'
-import Image from 'next/imatge'
+import Image from 'next/image'
 import { Box, LinkBox, LinkOverlay, Text } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 
-export const GridItem = ({ children, href, title, thumbnail }) => {
-  ;<Box w="100%" textAlign="center">
+export const GridItem = ({ children, href, title, thumbnail }) => (
+  <Box w="100%" textAlign="center">
     <LinkBox cursor="pointer">
       <Image
         src={thumbnail}
         alt={title}
-        clasName="grid-item-thumbnail"
+        className="grid-item-thumbnail"
         placeholder="blur"
         loading="lazy"
       />
@@ -19,10 +19,10 @@ export const GridItem = ({ children, href, title, thumbnail }) => {
       <Text fontSize={14}>{children}</Text>
     </LinkBox>
   </Box>
-}
+)
 
-export const WorkGridItem = ({ children, id, title, thumbnail }) => {
-  ;<Box w="100%" textAlign="center">
+export const WorkGridItem = ({ children, id, title, thumbnail }) => (
+  <Box w="100%" textAlign="center">
     <LinkBox
       as={NextLink}
       href={`/works/${id}`}
@@ -43,7 +43,7 @@ export const WorkGridItem = ({ children, id, title, thumbnail }) => {
       <Text fontSize={14}>{children}</Text>
     </LinkBox>
   </Box>
-}
+)
 
 export const GridItemStyle = () => (
   <Global
--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,9 @@
 import { Container, Divider, Heading, SimpleGrid } from '@chakra-ui/react'
-// import Section from '../components/Sextion'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+
+import thumbWebshop from '../public/works/admin.png'
+import thumbbatatabit from '../public/works/desktop.png'
 
 const Works = () => {
   return (
@@ -7,6 +11,28 @@ const Works = () => {
       <Heading as="h3" fontSize={20} mb={4}>
         Works
       </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} spacing={6}>
+        <Section>
+          <WorkGridItem id="admin" title="Admin" thumbnail={thumbWebshop}>
+            Yard Sales is a webshop application that allows users to buy and
+            sell items. It is built with React, Redux, Node.js, Express,
+            MongoDB, and Firebase.
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            id="batatabit"
+            title="Batatabit"
+            thumbnail={thumbbatatabit}
+          >
+            Batatabit is a responsive website that provides real-time
+            information about cryptocurrencies. The website is designed a
+            Mobile-First approach, ensuring it looks and functions seamlessly on
+            various devices, including smartphones, tablets, and desktops.
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
     </Container>
   )
 }
